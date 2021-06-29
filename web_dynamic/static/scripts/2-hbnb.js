621080d618e87e21444e1ee15a31e64448d71580
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-29 19:33:31 +0000

    Fixed semistandard errors and fixed route

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,3 +17,11 @@ $(document).ready(function(){
    $('.amenities h4').html(arr.join(', '));
   });
 });
+
+$.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
